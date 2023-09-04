ee54f5b2d1bc9886fa9eee3c440e7d2846b9478a
khalid-jatri <khalidhossain.jatri@gmail.com>
2023-09-04 12:44:53 +0600

    last n char string added

---
+++ 06.lastNcharString.js
@@ -0,0 +1,19 @@
+// Get last n characters of string
+// Write a function that takes a string as argument. Extract the last 3 characters from the string. Return the result
+
+//using slice
+// const findLastChar = (str) => {
+//     let result;
+//     result = str.slice(str.length - 4);
+//     return result;
+// }
+// console.log(findLastChar('Khalid Hossain'));
+
+
+// using substring
+const findLastChar = (str) => {
+    let result;
+    result = str.substring(str.length - 3);
+    return result;
+}
+console.log(findLastChar('Khalid Hossain'));
