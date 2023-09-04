2e02ef6d89899670ead19602879be88fa21cb1a6
khalid-jatri <khalidhossain.jatri@gmail.com>
2023-09-04 12:35:00 +0600

    remove n char from string added

--- 05.removeNcharString.js
+++ 05.removeNcharString.js
@@ -1,8 +1,26 @@
 // Remove first n characters of string
 // Write a function that takes a string (a) as argument. Remove the first 3 characters of a. Return the result
 
+//using slice
 const removeCharacter = (a) => {
-    const str = a.slice(3);
+    let str;
+    str = a.slice(3);
     return str;
 }
 console.log(removeCharacter("Khalid Hossain"));
+
+// using substring
+// const removeCharacter = (a) => {
+//     let str;
+//     str = a.substring(3);
+//     return str;
+// }
+// console.log(removeCharacter("Khalid Hossain"));
+
+//using replace
+// const removeCharacter = (a) => {
+//     let str;
+//     str = a.replace(/.{3}/, '');
+//     return str;
+// }
+// console.log(removeCharacter('Khalid Hossain'));
