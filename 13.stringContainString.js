1a998e6540301a8899463b93a9337e9cacc6507e
khalid-jatri <khalidhossain.jatri@gmail.com>
2023-09-05 12:14:16 +0600

    string contain string file updated

--- 13.stringContainString.js
+++ 13.stringContainString.js
@@ -2,10 +2,23 @@
 // Write a function that takes two strings (a and b) as arguments. If a contains b, append b to the beginning of a.
 // If not, append it to the end. Return the concatenation
 
+
 const checkString = (a, b) => {
-    if(a === b) {
-        return b.concat(a)
-    }
+    // return a.includes(b) ? b + a : a + b;
+    return a.indexOf(b) === -1 ? a + b : b + a
 }
 
-checkString(1, 2);
+// const checkString = (a, b) => {
+//     if(a.includes(b)) {
+//         return b + a;
+//     }
+//     else {
+//         return a + b;
+//     }
+// }
+
+console.log(checkString('cheese', 'cake'));
+console.log(checkString('lips', 's'));
+console.log(checkString('Java', 'script'));
+console.log(checkString(' think, therefore I am', 'I'));
+console.log(checkString('21', 2));
