37f038189d182bcf0e1135e5ad64ee8c5ae7d915
Khalid-Hossain-Tarif <khalidhossain1311@gmail.com>
2023-09-07 11:19:16 +0600

    14. even number file added and modified

--- 14.even-number.js
+++ 14.even-number.js
@@ -1,2 +1,20 @@
 // Check if a number is even
 // Write a function that takes a number as argument. If the number is even, return true. Otherwise, return false
+
+
+
+const evenNumber = (a) => {
+    return a % 2 === 0
+    // return a % 2 === 0 ? true : false;
+}
+
+// const evenNumber = (a) => {
+//     if(a % 2 === 0) {
+//         return true;
+//     }
+//     else {
+//         return false;
+//     }
+// }
+
+console.log(evenNumber(-111));
