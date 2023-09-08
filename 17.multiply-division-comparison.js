ed1ed3f170f64ae7eceb439a109542381e3f45d3
Khalid-Hossain-Tarif <khalidhossain1311@gmail.com>
2023-09-08 21:16:50 +0600

    multiply-division-comparison added and updated

---
+++ 17.multiply-division-comparison.js
@@ -0,0 +1,16 @@
+// Multiplication, division, and comparison operators
+// Write a function that takes two numbers (a and b) as arguments. 
+// If a is smaller than b, divide a by b. Otherwise, multiply both numbers. Return the resulting value
+
+const operators = (a, b) => {
+    return a < b ? a / b : a * b;
+
+    // if(a < b) {
+    //     return a / b;
+    // }
+    // else {
+    //     return a * b;
+    // }
+}
+
+console.log(operators(5, 0.5));
