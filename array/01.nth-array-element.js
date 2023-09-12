db43fea70fb7848d9d81d67204b5d90803a189f2
khalid-jatri <khalidhossain.jatri@gmail.com>
2023-09-12 15:29:36 +0600

    nth element of an array problem solved

--- array/01.nth-array-element.js
+++ array/01.nth-array-element.js
@@ -1,8 +1,10 @@
 // Get nth element of array
 // Write a function that takes an array (a) and a value (n) as argument. Return the nth element of 'a'
 
-const getNthElementOfArray = (a, n) => {
-    return a.indexOf(n)
+const nthArrayElementFinder = (a, n) => {
+    return a[n - 1];
 }
 
-console.log(getNthElementOfArray([1,2,3,4,5],3);
+console.log(nthArrayElementFinder([1,2,3,4,5],3));
+console.log(nthArrayElementFinder([10,9,8,7,6],5));
+
