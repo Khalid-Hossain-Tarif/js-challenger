a1661098ec1442079801c4178f313691835cf107
khalid-jatri <khalidhossain.jatri@gmail.com>
2023-09-13 13:01:39 +0600

    array length checke porblem solved

---
+++ array/07.array-length-check.js
@@ -0,0 +1,10 @@
+// Count number of elements in JavaScript array
+// Write a function that takes an array (a) as argument. Return the number of elements in a.
+
+const arrayLengthChecker = (a) => {
+    return a.length;
+}
+
+console.log(arrayLengthChecker([1,2,2,4])); //4
+console.log(arrayLengthChecker([9,9,9])); //3
+console.log(arrayLengthChecker([4,3,2,1,0])); //5
