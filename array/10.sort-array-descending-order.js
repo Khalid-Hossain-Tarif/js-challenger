506b3785bd4f0058c95d11c31d7d919508728c65
khalid-jatri <khalidhossain.jatri@gmail.com>
2023-09-14 13:07:22 +0600

    array sort in descending order - problem solved

--- array/10.sort-array-descending-order.js
+++ array/10.sort-array-descending-order.js
@@ -2,8 +2,8 @@
 // Write a function that takes an array of numbers as argument. It should return an array with the numbers sorted in descending order.
 
 const arrayDescendingOrder = (arr) => {
-    return arr;
+    return arr.sort((a, b) => b - a);
 }
 
 console.log(arrayDescendingOrder([1,3,2])); //[3,2,1]
-console.log(arrayDescendingOrder([4,2,3,1])); //[4,3,2,1]
+console.log(arrayDescendingOrder([4,2,-3,200])); //[200,4,2,-3]
