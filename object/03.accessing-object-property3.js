a3f2bb43b741f744b31b717b4b1745eff47c0cd6
khalid-jatri <khalidhossain1311@gmail.com>
2023-09-19 13:06:49 +0600

    accessing object property updated

--- object/03.accessing-object-property3.js
+++ object/03.accessing-object-property3.js
@@ -2,7 +2,7 @@
 // Write a function that takes an object with two properties and a string as arguments. It should return the value of the property with key equal to the value of the string
 
 const getObjectProperty = (obj, key) => {
-    return obj;
+    return obj
 }
 
 console.log(getObjectProperty({ continent: 'Asia',  country: 'Japan'}, 'continent')); //'Asia'
