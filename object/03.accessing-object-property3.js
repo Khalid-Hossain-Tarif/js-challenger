cab296b0b0d9873810f9c21d819176f03bdef4a7
khalid-jatri <khalidhossain1311@gmail.com>
2023-09-26 13:03:29 +0600

    accessing object property with key match - problem solved

--- object/03.accessing-object-property3.js
+++ object/03.accessing-object-property3.js
@@ -2,7 +2,15 @@
 // Write a function that takes an object with two properties and a string as arguments. It should return the value of the property with key equal to the value of the string
 
 const getObjectProperty = (obj, key) => {
-    return obj
+    return obj[key];
+
+    //Another solution
+    // for(let i in obj) {
+    //     if(i === key) {
+    //         return obj[i]
+    //     }
+    // }
+    // return false;
 }
 
 console.log(getObjectProperty({ continent: 'Asia',  country: 'Japan'}, 'continent')); //'Asia'
