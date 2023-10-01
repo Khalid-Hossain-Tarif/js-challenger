2cefd71143d72915f9a983b7d5c96a7a55c23610
khalid-jatri <khalidhossain1311@gmail.com>
2023-10-01 16:44:11 +0600

    object property check - problem solved

--- object/04.object-property-check.js
+++ object/04.object-property-check.js
@@ -3,5 +3,16 @@
 // Return false otherwise. Hint: test case 3 is a bit tricky because the value of property 'z' is undefined. But the property itself exists.
 
 const objPropertyCheck = (a, b) => {
+    return b in a;
 
+    // for(let i in a) {
+    //     if(i === b) {
+    //         return true
+    //     }
+    // }
+    // return false;
 }
+
+console.log(objPropertyCheck({a:1,b:2,c:3},'b')); //true
+console.log(objPropertyCheck({x:'a',y:'b',z:'c'},'a')); //false
+console.log(objPropertyCheck({x:'a',y:'b',z:undefined},'z')); //true
