7aef5e6d1a92c84d2ba3ce9671ab57274aa6f131
khalid-jatri <khalidhossain1311@gmail.com>
2023-10-01 17:53:54 +0600

    string to object - problem added

---
+++ object/06.string-to-object.js
@@ -0,0 +1,10 @@
+// Creating Javascript objects one
+// Write a function that takes a string as argument. Create an object that has a property with key 'key' and a value equal to the string. Return the object.
+
+const stringToObject = (a) => {
+    return a.key = a;
+}
+
+console.log(stringToObject('a')); //{key:'a'}
+console.log(stringToObject('z')); //{key:'z'}
+console.log(stringToObject('b')); //{key:'b'}
