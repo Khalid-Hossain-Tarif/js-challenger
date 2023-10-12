0f0c2c61cd7783335704ac6bb13e00eca41cccb2
khalid-jatri <khalidhossain1311@gmail.com>
2023-10-12 17:28:19 +0600

    sets check  value problem added

---
+++ sets/01.check-value-in-sets.js
@@ -0,0 +1,11 @@
+// Check if value is present in Set
+// Write a function that takes a Set and a value as arguments. Check if the value is present in the Set
+
+const checkValueInSets = (set, val) => {
+    return set;
+}
+
+console.log(checkValueInSets(new Set([1, 2, 3]), 2)); //true
+console.log(checkValueInSets(new Set([123]), 2)); //false
+console.log(checkValueInSets(new Set(['1', '2', '3']), '2')); //true
+console.log(checkValueInSets(new Set('123'), '2')); //true
