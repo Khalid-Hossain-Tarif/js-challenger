27311d5b18080baa3a43937f5d377369eb85b323
khalid-jatri <khalidhossain1311@gmail.com>
2023-11-06 12:00:10 +0600

    check if value is present in set - problem solved

--- sets/01.check-value-in-sets.js
+++ sets/01.check-value-in-sets.js
@@ -2,7 +2,7 @@
 // Write a function that takes a Set and a value as arguments. Check if the value is present in the Set
 
 const checkValueInSets = (set, val) => {
-    return set;
+    return set.has(val);
 }
 
 
